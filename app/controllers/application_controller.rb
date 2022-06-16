8709e029b879244e7ba4b2cab9e052235cbee672
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-06-16 18:41:56 +0300

    devise, and model User(admin)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  helper_method :current_user,
+                :logged_in?
+
+  def after_sign_in_path_for(resource)
+    if resource
+      admin_posts_path
+    else
+      redirect_to posts_path
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, presence: true
+  def is_admin?
+    self.is_a?(Admin)
+  end
+end
